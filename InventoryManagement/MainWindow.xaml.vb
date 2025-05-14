da75595a43664224a9f5212ab73592b09390b4be
Ibtisam'sDevAcc <lolimdinside@gmail.com>
2025-05-14 23:46:25 +0530

    Add project files.

---
+++ InventoryManagement/MainWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim NewItemWin As New NewItemWindow
+        NewItemWin.ShowDialog()
+    End Sub
+End Class
