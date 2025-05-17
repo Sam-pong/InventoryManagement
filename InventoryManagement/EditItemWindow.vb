e8ba9dc8c1127b39712064800d14e766c4456165
Ibtisam'sDevAcc <lolimdinside@gmail.com>
2025-05-17 19:40:59 +0530

    SO MUCH SHI.. i added a way to add items, add tags. it should work with barcode machines but i havent used one of those so no promises. Better UI i think. Maybe not but i think its useable. Next is a way to edit items, view items, maybe a lotta tooltips etc

--- InventoryManagement/EditItemWindow.vb
+++ InventoryManagement/EditItemWindow.vb
@@ -6,15 +6,7 @@ Public Class NewItemWindow
     End Sub
 
     Sub FillTAGCOMBO()
-        TAGCOMBO.ItemsSource = Nothing
         TAGCOMBO.Items.Clear()
-        nametxt.Text = Nothing
-        desctxt.Text = Nothing
-        locationtxt.Text = Nothing
-        purchasetxt.Text = Nothing
-        retailtxt.Text = Nothing
-        barcodetxt.Text = Nothing
-        stocktxt.Text = Nothing
 
         Using con As New SQLiteConnection(CStr("Data source = InvenManage.db"))
             Using com As New SQLiteCommand("SELECT * FROM TAGS", con)
@@ -26,7 +18,7 @@ Public Class NewItemWindow
                     ad.Fill(dt)
                     TAGCOMBO.ItemsSource = dt.DefaultView
                     TAGCOMBO.DisplayMemberPath = "TAGNAME"
-                    TAGCOMBO.SelectedValuePath = "TAGID"
+                    TAGCOMBO.SelectedValue = "TAGID"
                 Catch ex As SQLiteException
                     MsgBox("SQL Error: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
 
@@ -47,16 +39,15 @@ Public Class NewItemWindow
                 Try
                     con.Open()
 
-                    Using ad As New SQLiteDataAdapter(com)
-                        Dim dt As New DataTable
-                        ad.Fill(dt)
+                    Using RDR = com.ExecuteReader
+                        If RDR.HasRows Then
+                            Do While RDR.Read
+                                Dim ItemIDINT As Integer
+                                ItemIDINT = RDR.Item("Max(SerielNumber)")
 
-                        If dt.Rows.Count > 0 Then
-                            Dim ItemIDINT As Integer
-                            ItemIDINT = dt.Rows.Item(0)(0).ToString
+                                ItemID.Text = ItemIDINT + 1
 
-                            ItemID.Text = ItemIDINT + 1
-
+                            Loop
                         Else
                             ItemID.Text = 1
                         End If
@@ -92,58 +83,42 @@ Public Class NewItemWindow
             nametxt.Focus()
         ElseIf desctxt.Text = Nothing Then
             desctxt.Focus()
-        ElseIf TAGCOMBO.SelectedIndex < 0 Then
-            TAGCOMBO.Focus()
         ElseIf locationtxt.Text = Nothing Then
             locationtxt.Focus()
-
-
-        ElseIf stocktxt.Text = Nothing Then
-            stocktxt.Focus()
-
         ElseIf purchasetxt.Text = Nothing Then
             purchasetxt.Focus()
         ElseIf retailtxt.Text = Nothing Then
             retailtxt.Focus()
-
         ElseIf barcodetxt.Text = Nothing Then
             barcodetxt.Focus()
-        Else
-            Using con As New SQLiteConnection(CStr("Data source = InvenManage.db"))
-                Try
-
-
-                    Using cmd As New SQLiteCommand("INSERT INTO ITEMS (ItemName, Desc, TagID, Location, Stock, PurchasePrice, RetailPrice, Barcode) VALUES 
-(@NameItem, @Description, @IDTag, @Loc, @Stock, @Purchase, @Retail, @Barc)", con)
-                        cmd.Parameters.AddWithValue("@NameItem", nametxt.Text)
-                        cmd.Parameters.AddWithValue("@Description", desctxt.Text)
-                        cmd.Parameters.AddWithValue("@IDTag", TAGCOMBO.SelectedValue)
-                        cmd.Parameters.AddWithValue("@Loc", locationtxt.Text)
-                        cmd.Parameters.AddWithValue("@Stock", stocktxt.Text)
-                        cmd.Parameters.AddWithValue("@Purchase", purchasetxt.Text)
-                        cmd.Parameters.AddWithValue("@Retail", retailtxt.Text)
-                        cmd.Parameters.AddWithValue("@Barc", barcodetxt.Text)
-
-                        con.Open()
-                        cmd.ExecuteNonQuery()
-                        MsgBox("Success!")
-                        FillTAGCOMBO()
-                        ItemIDFill()
-
-                        con.Close()
-                    End Using
-                Catch ex As SQLiteException
-                    MsgBox("SQL Error: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
-
-                Catch ex As Exception
-                    MsgBox("Unexpected Error: " & ex.Message, MsgBoxStyle.Critical, "Error")
-
-                End Try
-            End Using
-
         End If
+        Using con As New SQLiteConnection(CStr("Data source = InvenManage.db"))
+            Try
 
 
+                Using cmd As New SQLiteCommand("INSERT INTO ITEMS (ItemName, Desc, TagID, Location, Stock, PurchasePrice, RetailPrice, Barcode) VALUES 
+(@NameItem, @Description, @IDTag, @Loc, @Stock, @Purchase, @Retail, @Barc)", con)
+                    cmd.Parameters.AddWithValue("@NameItem", nametxt.Text)
+                    cmd.Parameters.AddWithValue("@Description", desctxt.Text)
+                    cmd.Parameters.AddWithValue("@IDTag", TAGCOMBO.SelectedValue)
+                    cmd.Parameters.AddWithValue("@Loc", locationtxt.Text)
+                    cmd.Parameters.AddWithValue("@Stock", stocktxt.Text)
+                    cmd.Parameters.AddWithValue("@Purchace", purchasetxt.Text)
+                    cmd.Parameters.AddWithValue("@Retail", retailtxt.Text)
+                    cmd.Parameters.AddWithValue("@Barc", barcodetxt.Text)
+
+                    con.Open()
+                    cmd.ExecuteNonQuery()
+                    con.Close()
+                End Using
+            Catch ex As SQLiteException
+                MsgBox("SQL Error: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
+
+            Catch ex As Exception
+                MsgBox("Unexpected Error: " & ex.Message, MsgBoxStyle.Critical, "Error")
+
+            End Try
+        End Using
 
     End Sub
 End Class
---
+++ InventoryManagement/NewTag.xaml.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Data.SQLite
+Public Class NewTag
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+    Sub loadID()
+        Using con As New SQLiteConnection(CStr("Data source = InvenManage.db"))
+            TagName.Text = Nothing
+            Try
+                con.Open()
+                Using com As New SQLiteCommand("SELECT max(TagID) FROM TAGS", con)
+                    Using RDR = com.ExecuteReader
+                        If RDR.HasRows Then
+                            Do While RDR.Read
+                                Dim TagIDint As Integer
+                                TagIDint = RDR.Item("max(TagID)")
+
+                                TagID.Text = TagIDint + 1
+
+                            Loop
+                        Else
+                            TagID.Text = "1"
+                        End If
+
+                    End Using
+
+                End Using
+
+            Catch ex As SQLiteException
+                MsgBox("SQL Error: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
+
+            Catch ex As Exception
+                MsgBox("Unexpected Error: " & ex.Message, MsgBoxStyle.Critical, "Error")
+
+            End Try
+            con.Close()
+
+        End Using
+    End Sub
+    Private Sub NewTag_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        loadID()
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        If TagName.Text = Nothing Then
+            TagName.Focus()
+        Else
+            Using con As New SQLiteConnection(CStr("Data source = InvenManage.db"))
+                Using com As New SQLiteCommand("INSERT INTO TAGS (TagName) VALUES (@TagNameParam)", con)
+                    com.Parameters.AddWithValue("@TagNameParam", TagName.Text)
+                    con.Open()
+                    Try
+                        com.ExecuteNonQuery()
+                        loadID()
+                    Catch ex As SQLiteException
+                        MsgBox("SQL Error: " & ex.Message, MsgBoxStyle.Critical, "Database Error")
+
+                    Catch ex As Exception
+                        MsgBox("Unexpected Error: " & ex.Message, MsgBoxStyle.Critical, "Error")
+
+                    End Try
+                    con.Close()
+                End Using
+            End Using
+
+        End If
+    End Sub
+End Class
